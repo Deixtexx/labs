6c3e15ae0f63401cd8a61dcaaa2a918baf4597d3
akune08 <orlovsky_platon@mail.ru>
2026-05-01 13:26:02 +0600

    Create task4.h

---
+++ task4.h
@@ -0,0 +1,12 @@
+#ifndef QUADRATIC_H
+#define QUADRATIC_H
+
+typedef struct {
+    int count;       
+    double roots[2]; 
+} QuadraticResult;
+
+QuadraticResult solve_quadratic(double a, double b, double c);
+
+#endif
+
