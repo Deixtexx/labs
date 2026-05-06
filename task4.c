97547cd8753ef8937b8e9ff7892373f5df22514d
akune08 <orlovsky_platon@mail.ru>
2026-05-06 20:06:13 +0600

    Update task4.c

--- task4.c
+++ task4.c
@@ -1,23 +1,28 @@
 #include "task4.h"
 #include <math.h>
-#include <float.h>
-
-#define EPS 1e-7
 
 QuadraticResult solve_quadratic(double a, double b, double c) {
     QuadraticResult res = { 0, {0.0, 0.0} };
 
-    if (fabs(a) < DBL_EPSILON) {
-        res.count = -1; 
+    if (fabs(a) < 1e-15) {
+        res.count = -1;
         return res;
     }
 
-    double discriminant = b * b - 4 * a * c;
+    double d = b * b - 4 * a * c;
 
-    if (discriminant > EPS) {
+    if (d < 1e-7 && d > -1e-7) {
+        res.count = 1;
+        res.roots[0] = -b / (2 * a);
+        if (fabs(res.roots[0]) < 1e-12)
+            res.roots[0] = 0.0;
+    }
+    else if (d > 0) {
         res.count = 2;
-        res.roots[0] = (-b - sqrt(discriminant)) / (2 * a);
-        res.roots[1] = (-b + sqrt(discriminant)) / (2 * a);
+        double sd = sqrt(d);
+        double q = -0.5 * (b + (b >= 0 ? 1.0 : -1.0) * sd);
+        res.roots[0] = q / a;
+        res.roots[1] = c / q;
 
         if (res.roots[0] > res.roots[1]) {
             double tmp = res.roots[0];
@@ -25,14 +30,10 @@ QuadraticResult solve_quadratic(double a, double b, double c) {
             res.roots[1] = tmp;
         }
     }
-    else if (discriminant > -EPS) {
-        res.count = 1;
-        res.roots[0] = -b / (2 * a);
-        if (fabs(res.roots[0]) < EPS) res.roots[0] = 0.0;
-    }
     else {
         res.count = 0;
     }
 
     return res;
 }
+
